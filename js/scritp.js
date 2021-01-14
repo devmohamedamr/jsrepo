bd8a3c3eb65e8a4c807238185bf9627605efd9ce
Mohamed Amr <dev.mohamedamr@gmail.com>
2021-01-14 19:46:24 +0200

    first push

---
+++ js/scritp.js
@@ -0,0 +1,180 @@
+
+var x = 10,
+y = 20;
+
+var age = 50;
+// var res = x < y;
+// console.log(res);
+
+// if(x < y){
+//     console.log("x < y");
+// }else if (age == 40){
+//     console.log("age is 30");
+// }else{
+//     console.log("error");
+// }
+
+
+// if(age == 40){
+//     console.log("age is 40");
+// }else if (age == 30){
+//     console.log("age is 30");
+
+// }
+
+
+// switch(age){
+//     case 40:
+//         console.log("age is 40");
+//     break;
+//     case 30:
+//         console.log("age is 30");
+//     break; 
+//     default :
+//     console.log("error");
+//     break;
+// }
+
+
+
+
+// function cls(){
+//     var x = Number(document.getElementById("x").value);
+//     var y = Number(document.getElementById("y").value);
+
+
+//     var op = document.getElementById("op").value;
+//     switch(op){
+//         case "+":
+//             var total = x + y;
+//         break;   
+//         case "-":
+//             var total = x - y;
+//         break;  
+//         case "*":
+//             var total = x * y;
+//         break;  
+//         case "/":
+//             var total = x / y;
+//         break;   
+//     }
+
+//     document.getElementById("res").innerHTML = "your res is : "+total;
+// }
+
+
+// var x = 0;
+// do{
+   
+//     document.write(x+"<br>");
+//     x++;
+// }while(10<x);
+   
+  
+
+// for(var x = 0;10>x;x++){
+//     document.write(x+"<br>");
+// }
+
+
+
+
+
+// function welcome(){
+//     document.write("welcome ya mohamed <br>");
+// } 
+
+// welcome();
+// welcome();
+
+
+
+// function n(x){
+//     // console.log(20);
+//     return x;
+// }
+
+// n();
+
+// console.log( 30 + n(10));
+
+// console.log( 30 + n(30));
+
+
+
+// function w(){
+//     console.log('test');
+// }
+
+
+// var w = function(){
+//     console.log('test');
+
+// }
+
+// w();
+
+
+
+
+// var person = {name : "mohamed",age : 25 , 10 : 20 , "10":30};
+
+// console.log(person);
+
+// var names = ["mohamed","ahmed","radwa"];
+// console.log(names);
+// document.getElementById("names").innerHTML = names;
+// var list = '';
+// // for(var i =0;i<names.length;i++){
+//     names.forEach( function(n,s){
+//         list += "<li>" +s+ "</li>";
+
+//     } );
+
+// // }
+
+
+
+// document.getElementById("names").innerHTML = list;
+
+
+
+
+
+// var names = ["mohamed","ahmed","eslam"];
+
+// // for(var i = 0;i<names.length;i++){
+// //     console.log(names[i]);
+// // }
+
+// names.forEach( function(value,key){
+//     console.log(key);
+
+// } );
+
+
+
+
+// console.log(document.getElementById("test"));
+
+
+// console.log(document.getElementsByClassName("test"));
+
+
+// console.log(document.getElementsByTagName("p"));
+
+
+// document.getElementById("test").innerText = "<h1>www</h1>"
+
+
+// document.getElementById("test").style. = "red";
+
+
+
+
+// document.getElementById("name").onkeydown = function(e){
+//     console.log(e);
+// }
+
+
+document.getElementById("test").classList.remove("text");
